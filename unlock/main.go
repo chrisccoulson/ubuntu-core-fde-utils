411c20b6bf5a551592fc69e70e26a525b3d328e1
Chris Coulson <chris.coulson@canonical.com>
2020-01-08 23:03:57 +0000

    Make unlock call cryptsetup with a passphrase rather than master key
    Traditional LUKS setups encrypt the volume key to one or more keyslots
    using an intermediate key that's derived from a passphrase. In an ideal
    world when sealing the disk encryption key to the TPM, the secret that
    would be encrypted would be the intermediate key (so, the TPM would unseal
    the intermediate key, and the intermediate key would be used to decrypt the
    volume key from a keyslot).
    
    However, keyslots in LUKS are really tied to passphrases. The KDFs that
    derive the intermediate keys from passphrases are slow and resource
    intensive, to harden the system against brute-forcing low-entropy
    passphrases. As we can seal an arbitrarily long key to the TPM rather
    than a low-entropy passphrase, we don't need the overhead of a resource
    intensive KDF, and we don't want to block the boot for a couple of seconds
    whilst the KDF derives the intermediate key from a secret unsealed from the
    TPM. One option could have been to configure LUKS with a passphrase/keyslot
    and then seal the intermediate key derived from the passphrase to the TPM,
    but there is no support in cryptsetup to do this. For the initial plan for
    Ubuntu Core, we opted to just seal the volume key to the TPM instead, which
    can be used to open LUKS volumes in cryptsetup.
    
    However, opening a volume with the volume key is not supported in
    systemd-cryptsetup, which is what we want in the initramfs rather than the
    full cryptsetup utility. To support systemd-cryptsetup, we can configure
    the LUKS volume with a keyslot and high-entropy passphrase (say, a 64
    byte key), but configure the KDF to execute with minimal overhead so that
    it derives the intermediate key quickly.
    
    This commit updates the unlock tool to open the LUKS volume with a
    passphrase, which is really a high-entropy key.

--- unlock/main.go
+++ unlock/main.go
@@ -20,6 +20,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
@@ -35,10 +36,6 @@ var insecure bool
 var ekCertFile string
 var keyFile string
 var pin string
-
-const (
-	masterKeyFilePath string = "/run/unlock.tmp"
-)
 
 const (
 	unspecifiedErrorExitCode = iota + 1
@@ -163,23 +160,9 @@ RetryUnseal:
 		return ret
 	}
 
-	masterKeyFile, err := os.OpenFile(masterKeyFilePath, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0600)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Cannot unlock device %s: error creating temporary master key file: %v", devicePath, err)
-		return unspecifiedErrorExitCode
-	}
-	defer func() {
-		defer os.Remove(masterKeyFilePath)
-		defer masterKeyFile.Close()
-	}()
-
-	if _, err := masterKeyFile.Write(key); err != nil {
-		fmt.Fprintf(os.Stderr, "Cannot unlock device %s: error writing master key to temporary file: %v", devicePath, err)
-		return unspecifiedErrorExitCode
-	}
-
-	cmd := exec.Command("cryptsetup", "--type", "luks2", "--master-key-file", masterKeyFilePath, "open", devicePath, name)
+	cmd := exec.Command("cryptsetup", "open", "--type", "luks2", "--key-file", "-", devicePath, name)
 	cmd.Env = append(os.Environ(), "LD_PRELOAD=/lib/no-udev.so")
+	cmd.Stdin = bytes.NewReader(key)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
